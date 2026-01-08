c9ed7896430b2aef6de94bff0092ce1eb31193e2
sh4d0w3lmyr4 <119040682+sh4d0w3lmyr4@users.noreply.github.com>
2026-01-08 20:17:52 +0100

    Discord bot

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
+import { Client, GatewayIntentBits } from "discord.js";
 
-const app = express();
-const PORT = 3000;
-
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Use the router for handling routes
-app.use('/', indexRouter);
-
-// Catch-all route for handling 404 errors
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  });
-
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ],
 });
+
+client.once("ready", () => {
+  console.log("Hardground BOT ONLINE 💀");
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("PONG 🔊 HARDGROUND LIVE");
+  }
+});
+
+client.login(process.env.TOKEN);
+
