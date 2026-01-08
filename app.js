c3f2580d7bd6b9d818b412bbf9c27157d7f1ca02
sh4d0w3lmyr4 <119040682+sh4d0w3lmyr4@users.noreply.github.com>
2026-01-08 22:21:40 +0100

    Update app.js

--- app.js
+++ app.js
@@ -216,7 +216,7 @@ client.on("messageCreate", async (message) => {
     );
     if (!channel) return message.reply("❌ Kanaal #regels niet gevonden.");
 
-await channel.send("https://media.discordapp.net/attachments/1101254205492179015/1458931424001265796/ChatGPT_Image_8_jan_2026_22_12_29.png?ex=69616f45&is=69601dc5&hm=6da91a28270a7035a7068f8f9376c5028a7490bf6eb9d1ea3da4ed34138ccbba&=&format=webp&quality=lossless&width=640&height=960");
+await channel.send("https://media.discordapp.net/attachments/1101254205492179015/1458933507442085929/ChatGPT_Image_8_jan_2026_22_20_41.png?ex=69617135&is=69601fb5&hm=574a26f9773a56170db15db53a31dfecc8bda9bed95c221aad781a143edf8cd3&=&format=webp&quality=lossless&width=525&height=350");
 
 
     await channel.send({ embeds: [embed] });
