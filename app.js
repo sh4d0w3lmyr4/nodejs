07c702ea690d5a5448b71fde569b4912f03d9ae0
sh4d0w3lmyr4 <119040682+sh4d0w3lmyr4@users.noreply.github.com>
2026-01-09 01:14:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-{
-  "name": "hardground-bot",
-  "version": "1.0.0",
-  "type": "module",
-  "main": "app.js",
-  "scripts": {
-    "start": "node app.js"
-  },
-  "dependencies": {
-    "discord.js": "^14.16.3"
-  }
-}
+
